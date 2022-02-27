ab41487474648283ad024a0c35ae5248496e84b9
jackfiled <xcrenchangjun@outlook.com>
2022-02-27 12:25:28 +0800

    底部导航栏切换完成

---
+++ lib/Widget/calendarHome.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CalendarHome extends StatelessWidget {
+  const CalendarHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("日历"),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:postcalendar/Widget/calendarHome.dart';
+import 'package:postcalendar/Widget/ddlHome.dart';
+import 'package:postcalendar/Widget/activityHome.dart';
+
 void main(){
   runApp(const MyApp());
 }
@@ -10,17 +14,78 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: "Welcome to flutter",
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Welcome to flutter"),
-        ),
-        body: const Center(
-          child: Text("Hello flutter!"),
-        ),
-      ),
+      home: IndexPage(),
     );
   }
 }
+
+class IndexPage extends StatefulWidget {
+  const IndexPage({Key? key}) : super(key: key);
+
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+
+  int currentIndex = 0;
+
+  //底部导航栏的选择对象列表
+  final List<BottomNavigationBarItem> bottomNavItems = [
+    const BottomNavigationBarItem(
+      backgroundColor: Colors.blue,
+      icon: Icon(Icons.calendar_today),
+      label: "日历",
+    ),
+    const BottomNavigationBarItem(
+      backgroundColor: Colors.blue,
+      icon: Icon(Icons.calendar_view_day),
+      label: "DDL",
+    ),
+    const BottomNavigationBarItem(
+      backgroundColor: Colors.blue,
+      icon: Icon(Icons.local_activity),
+      label: "活动",
+    )
+  ];
+
+  //需要展示的页面
+  final pages = [const CalendarHome(), const DDLHome(), const ActivityHome()];
+
+  @override
+  void initState() {
+    super.initState();
+    currentIndex = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("PostCalendar"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: bottomNavItems,
+        currentIndex: currentIndex,
+        type: BottomNavigationBarType.fixed,
+        onTap: (index){
+          _changePage(index);
+        },
+      ),
+      body: pages[currentIndex],
+    );
+  }
+
+  void _changePage(int index){
+    if(index != currentIndex){
+      setState(() {
+        currentIndex = index;
+      });
+    }
+  }
+
+}
+
 
