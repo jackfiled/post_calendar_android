2698c4d9c3891788bff78a9d10adee474d7d19b1
jackfiled <xcrenchangjun@outlook.com>
2022-03-10 11:41:10 +0800

    完成了日历中时间的显示

--- lib/ViewModel/CalendarHomeView.dart
+++ lib/ViewModel/CalendarHomeView.dart
@@ -1,6 +1,3 @@
-
-import 'package:flutter/material.dart';
-
 class CalendarHomeViewItem{
   var year = "";
   var monday = "";
@@ -16,6 +13,46 @@ class CalendarHomeViewItem{
 
   CalendarHomeViewItem(DateTime timeOfFirstDay){
     date = timeOfFirstDay;
+
+    year = "${date.year}";
+    sunday = "${date.month}-${date.day}";
+    date = date.add(oneDay);
+    monday = "${date.month}-${date.day}";
+    date = date.add(oneDay);
+    tuesday = "${date.month}-${date.day}";
+    date = date.add(oneDay);
+    wednesday = "${date.month}-${date.day}";
+    date = date.add(oneDay);
+    thursday = "${date.month}-${date.day}";
+    date = date.add(oneDay);
+    friday = "${date.month}-${date.day}";
+    date = date.add(oneDay);
+    saturday = "${date.month}-${date.day}";
+  }
+
+  //切换到下一周
+  void nextWeek(){
+    date.add(const Duration(days: 7));
+
+    year = "${date.year}";
+    sunday = "${date.month}-${date.day}";
+    date = date.add(oneDay);
+    monday = "${date.month}-${date.day}";
+    date = date.add(oneDay);
+    tuesday = "${date.month}-${date.day}";
+    date = date.add(oneDay);
+    wednesday = "${date.month}-${date.day}";
+    date = date.add(oneDay);
+    thursday = "${date.month}-${date.day}";
+    date = date.add(oneDay);
+    friday = "${date.month}-${date.day}";
+    date = date.add(oneDay);
+    saturday = "${date.month}-${date.day}";
+  }
+
+  //切换到上一周
+  void lastWeek(){
+    date.add(const Duration(days: -7));
 
     year = "${date.year}";
     sunday = "${date.month}-${date.day}";
--- lib/Widget/calendarHome.dart
+++ lib/Widget/calendarHome.dart
@@ -15,6 +15,8 @@ class _CalendarHomeState extends State<CalendarHome> {
   late Widget head;
   late Widget calendarBody;
 
+  static const double timeColumnWidth = 60.0;
+
   @override
   void initState() {
     super.initState();
@@ -81,7 +83,7 @@ class _CalendarHomeState extends State<CalendarHome> {
     head = Table(
       //border: TableBorder.all(color: CupertinoColors.extraLightBackgroundGray),//不画边框可能会更更好一点
       columnWidths: const <int, TableColumnWidth>{
-        0: FlexColumnWidth(),
+        0: FixedColumnWidth(timeColumnWidth),
         1: FlexColumnWidth(),
         2: FlexColumnWidth(),
         3: FlexColumnWidth(),
@@ -154,29 +156,226 @@ class _CalendarHomeState extends State<CalendarHome> {
       ],
     );
 
-    calendarBody = Container(
-      child: ListView(
+    calendarBody = SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Table(
+        columnWidths: const <int, TableColumnWidth>{
+          0: FixedColumnWidth(timeColumnWidth),
+          1: FlexColumnWidth(),
+          2: FlexColumnWidth(),
+          3: FlexColumnWidth(),
+          4: FlexColumnWidth(),
+          5: FlexColumnWidth(),
+          6: FlexColumnWidth(),
+          7: FlexColumnWidth()
+        },
         children: [
-          Table(
-            columnWidths: const <int, TableColumnWidth>{
-              0: FixedColumnWidth(20),
-              1: FlexColumnWidth(),
-              2: FlexColumnWidth(),
-              3: FlexColumnWidth(),
-              4: FlexColumnWidth(),
-              5: FlexColumnWidth(),
-              6: FlexColumnWidth(),
-              7: FlexColumnWidth(),
-            },//设置表格中栏目的宽度
+          TableRow(
             children: [
-              TableRow(
-                  children: [
-                    Container(
-
-                    )
-                  ]
-              )
-            ],
+              Column(
+                children: [
+                  Container(
+                    child: const Center(
+                      child: Text("0:00-1:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("1:00-2:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("2:00-3:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("3:00-4:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("4:00-5:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("5:00-6:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("6:00-7:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("7:00-8:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("8:00-9:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("9:00-10:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("10:00-11:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("11:00-12:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("12:00-13:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("13:00-14:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("14:00-15:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("15:00-16:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("16:00-17:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("17:00-18:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("18:00-19:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("19:00-20:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("20:00-21:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("21:00-22:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("22:00-23:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text("23:00-24:00"),
+                    ),
+                    height: 50,
+                    width: timeColumnWidth,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+              Column(),
+              Column(),
+              Column(),
+              Column(),
+              Column(),
+              Column(),
+              Column(),
+            ]
           )
         ],
       ),
@@ -187,9 +386,12 @@ class _CalendarHomeState extends State<CalendarHome> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             head,
-            calendarBody,
+            Expanded(
+              child: calendarBody
+            ),
           ],
         )
     );
