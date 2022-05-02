36c044c989c2c44194652057afb72a5a5dd34a6e
jackfiled <xcrenchangjun@outlook.com>
2022-05-02 15:46:22 +0800

    将时间栏独立为一个文件

---
+++ lib/Widget/Calendar/calendar_time_column_widget.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:post_calendar_android/Common/global.dart';
+
+/// 日历左侧的时间
+class CalendarTimeColumn extends StatelessWidget {
+  const CalendarTimeColumn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          child: const Center(
+            child: Text("0:00-1:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("1:00-2:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("2:00-3:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("3:00-4:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("4:00-5:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("5:00-6:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("6:00-7:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("7:00-8:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("8:00-9:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("9:00-10:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("10:00-11:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("11:00-12:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("12:00-13:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("13:00-14:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("14:00-15:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("15:00-16:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("16:00-17:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("17:00-18:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("18:00-19:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("19:00-20:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("20:00-21:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("21:00-22:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("22:00-23:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+        Container(
+          child: const Center(
+            child: Text("23:00-24:00"),
+          ),
+          height: CalendarSetting.columnHeight,
+          width: CalendarSetting.columnWidth,
+          color: Colors.blue,
+        ),
+      ],
+    );
+  }
+}
