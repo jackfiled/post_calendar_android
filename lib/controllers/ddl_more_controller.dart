4d02de4e3872c183805afc39290a624cbdcfce30
jackfiled <xcrenchangjun@outlook.com>
2022-07-07 14:51:11 +0800

    添加DDL 详情界面

---
+++ lib/controllers/ddl_more_controller.dart
@@ -0,0 +1,54 @@
+import 'package:get/get.dart';
+
+import 'package:post_calendar_android/database/ddl_provider.dart';
+import 'package:post_calendar_android/data_structures/ddl_model.dart';
+
+class DDLMoreController extends GetxController{
+  final provider = DDLProvider.getInstance();
+  final _item = Rx<DDLModel?>(null);
+
+  Future<void> initContent(int id) async {
+    _item.value = await provider.read(id);
+  }
+
+  /// 名字字符串
+  String get name {
+    if(_item.value == null){
+      return "";
+    }else{
+      return _item.value!.name;
+    }
+  }
+
+  /// 地点字符串
+  String get place {
+    if(_item.value == null){
+      return "";
+    }else{
+      return _item.value!.place;
+    }
+  }
+
+  /// 详情字符串
+  String get details {
+    if(_item.value == null){
+      return "";
+    }else{
+      return _item.value!.details;
+    }
+  }
+
+  /// 结束时间字符串
+  String get endDateString {
+    if(_item.value == null){
+      return "";
+    }else{
+      return "${_item.value!.endTime.year}年"
+        "${_item.value!.endTime.month}月"
+        "${_item.value!.endTime.day}日"
+        "${_item.value!.endTime.hour}时"
+        "${_item.value!.endTime.minute}分"
+        "${_item.value!.endTime.second}秒";
+    }
+  }
+}
---
+++ lib/pages/ddl_more_page.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:post_calendar_android/routes/route_config.dart';
+import 'package:post_calendar_android/controllers/ddl_more_controller.dart';
+
+class DDLMorePage extends StatelessWidget {
+  DDLMorePage({Key? key}) : super(key: key);
+  
+  final _id = int.parse(Get.parameters['id']!);
+  final controller = Get.put(DDLMoreController());
+
+  final titleTextStyle = const TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.bold
+  );
+
+  final resultTextStyle = const TextStyle(
+    fontSize: 20,
+    color: Colors.grey,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    controller.initContent(_id);
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Get.back(),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.create),
+            onPressed: () => Get.toNamed(RouteConfig.ddlDetailPage + "?id=$_id"),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Text(
+                    controller.name,
+                    style: titleTextStyle
+                  ),
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "地点",
+                style: titleTextStyle,
+              ),
+              Obx(() => Text(
+                controller.place,
+                style: resultTextStyle,
+              ))
+            ],
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "结束时间",
+                style: titleTextStyle,
+              ),
+              Obx(() => Text(
+                controller.endDateString,
+                style: resultTextStyle,
+              ))
+            ],
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  "详情",
+                  style: titleTextStyle,
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Obx(() => Text(
+                  controller.details,
+                  style: resultTextStyle,
+                )),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
