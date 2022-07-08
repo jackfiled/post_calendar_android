566ebf68f0d777ffcfffb595b4cddc890f46d2b1
jackfiled <xcrenchangjun@outlook.com>
2022-07-08 14:43:08 +0800

    添加了DDL对象的左滑控制栏

--- lib/pages/ddl_page.dart
+++ lib/pages/ddl_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 import 'package:post_calendar_android/controllers/ddl_controller.dart';
-import 'package:post_calendar_android/components/ddl_item_cell.dart';
+import 'package:post_calendar_android/data_structures/ddl_model.dart';
 import 'package:post_calendar_android/routes/route_config.dart';
-
 
 /// DDL页面
 class DDLPage extends StatelessWidget {
@@ -25,9 +25,7 @@ class DDLPage extends StatelessWidget {
               itemBuilder: (context, index) {
                 final item = controller.ddlItems[index];
 
-                return DDLItemCell(
-                    item: item,
-                );
+                return _buildDDLItemCell(context, item);
               },
             )
         ),
@@ -38,6 +36,105 @@ class DDLPage extends StatelessWidget {
         child: const Icon(Icons.add),
         mini: true,
       ),
+    );
+  }
+
+  /// 绘制DDL事件单元
+  Widget _buildDDLItemCell(BuildContext context, DDLModel item) {
+    /// 设置标题的样式
+  const titleTextStyle = TextStyle(
+      color: Color.fromARGB(255, 0, 0, 0),
+      fontSize: 28,
+      fontWeight: FontWeight.bold);
+
+  /// 设置其他文字的样式
+  const otherTextStyle = TextStyle(
+      color: Color.fromARGB(122, 139, 136, 136),
+      fontSize: 20,
+      fontWeight: FontWeight.normal);
+
+    return Container(
+      margin: const EdgeInsets.all(10),
+      alignment: Alignment.centerLeft,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.all(Radius.circular(3)),
+        border: Border.all(width: 2, color: const Color.fromARGB(122, 123, 123, 123))
+      ),
+      child: Slidable(
+        key: const ValueKey(0),
+
+        endActionPane: ActionPane(
+          motion: const BehindMotion(),
+          children: [
+            SlidableAction(
+              // 完成按钮
+              onPressed: (context) {},
+              backgroundColor: Colors.green,
+              foregroundColor: Colors.white,
+              icon: Icons.check,
+            ),
+            SlidableAction(
+              // 编辑按钮
+              onPressed: (context) {
+                Get.toNamed(RouteConfig.ddlDetailPage + "?id=${item.id}");
+              },
+              backgroundColor: Colors.orange,
+              foregroundColor: Colors.white,
+              icon: Icons.create,
+            ),
+            SlidableAction(
+              // 删除按钮
+              onPressed: (context) {},
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+            )
+          ],
+        ),
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  item.name,
+                  style: titleTextStyle,
+                ),
+                Text(
+                  "${item.endTime.month}月${item.endTime.day}日",
+                  style: otherTextStyle,
+                )
+              ],
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 10),
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 3),
+                    child: Text(
+                      item.place,
+                      style: otherTextStyle,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis
+                    ),
+                  ),
+                  Text(
+                    item.details,
+                    style: otherTextStyle,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
